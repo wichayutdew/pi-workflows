79b9aae23ef6fdb08bdf6d563c9f31e32cf45079
Wichayut Phongphanpanya <34377409+wichayutdew@users.noreply.github.com>
2026-07-30 21:08:35 +0700

    fix: ignore markdown file from linting/formatting (#19)
    Co-authored-by: Wichayut Phongphanpanya <wichayut.phongphanpanya@agoda.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['coverage', 'dist', 'node_modules'],
+    ignores: ['coverage', 'dist', 'node_modules', '**/*.md'],
   },
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
